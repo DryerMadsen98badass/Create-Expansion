2d7f123c336abd01fa932ce1b65d00b91a8955c7
DryerMadsen98badass <furenesmads@gmail.com>
2026-05-28 22:30:05 +0200

    Create client setup class

---
+++ src/main/java/net/mads/createexpansion/client/ClientSetup.java
@@ -0,0 +1,16 @@
+package net.mads.createexpansion.client;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.mads.createexpansion.CreateExpansion;
+
+@Mod.EventBusSubscriber(modid = CreateExpansion.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientSetup {
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        // Client-side setup goes here
+    }
+}
